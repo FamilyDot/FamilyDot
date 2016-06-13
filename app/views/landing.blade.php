e9d89cb353c1fd9eb43cd948c81697c299a0869c
lo v <lov@los-MacBook-Pro.local>
2016-06-13 15:27:20 -0500

    the landing page is the sign up page now. very minimal layout

--- app/views/landing.blade.php
+++ app/views/landing.blade.php
@@ -1,32 +1,33 @@
 @extends('layouts.master')
 
 @section('topscript')
-
-<link rel="stylesheet"  href="">
+    <link rel="stylesheet"  href="">
 @stop
 
 
-@section('abovecontainer')
-<div id="Login">
-<h1>Login</h1>
+@section('content')
+<div id="link">
+    <h1><a href="login">Have an account?</h1>
 </div>
 
+<div id="signup-form">
+        {{-- the action is temporary need to go back and redo --}}
+    {{ Form::open(array('action' =>('HomeController@showLanding'), 'method' => 'POST')) }}
 
-{{ Form::open(array('action' =>('HomeController@showLanding'), 'method' => 'POST')) }}
 
-
-<div id="signup">
-    {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
-    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
-    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
-    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Confirm Password')) }}
-    {{ Form::text('dateOfBirth',  null,  array('class' =>'form-control', 'placeholder'=> 'Date Of Birth')) }}
-    {{ Form::text('family',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name')) }}
+    <div id="signup">
+        {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
+        {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
+        {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
+        {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Confirm Password')) }}
+        {{ Form::text('dateOfBirth',  null,  array('class' =>'form-control', 'placeholder'=> 'Date Of Birth')) }}
+        {{ Form::text('family',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name')) }}
+    </div>
+            <div id="button">
+                <input class="btn btn-success" id="btn_sub"  type="submit" value="Signup">
+            </div>
+    {{ Form::close() }}
 </div>
-        <div id="button">
-            <input class="btn btn-success" id="btn_sub"  type="submit" value="Login">
-        </div>
-{{ Form::close() }}
 @stop
 
 
