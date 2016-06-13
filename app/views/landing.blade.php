ad3cc3eca2044609653cab07941e7dc5187718a8
lo v <lov@los-MacBook-Pro.local>
2016-06-13 14:31:22 -0500

    deleted the landing folder and added it as a file in view

---
+++ app/views/landing.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('topscript')
+
+<link rel="stylesheet"  href="">
+@stop
+
+
+@section('abovecontainer')
+<div id="Login">
+<h1>Login</h1>
+</div>
+
+
+{{ Form::open(array('action' =>(''), 'method' => 'POST')) }}
+
+
+<div id="signup">
+    {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
+    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
+    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
+    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Confirm Password')) }}
+    {{ Form::text('dateOfBirth',  null,  array('class' =>'form-control', 'placeholder'=> 'Date Of Birth')) }}
+    {{ Form::text('family',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name')) }}
+</div>
+        <div id="button">
+            <input class="btn btn-success" id="btn_sub"  type="submit" value="Login">
+        </div>
+{{ Form::close() }}
+@stop
+
+
