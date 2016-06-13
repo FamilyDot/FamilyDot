5bce64afd6c09e42f2237958c948d27189634de3
lo v <lov@los-MacBook-Pro.local>
2016-06-13 14:11:59 -0500

    changes

--- app/views/landing/landingPage.blade.php
+++ app/views/landing/landingPage.blade.php
@@ -7,8 +7,13 @@
 
 
 @section('abovecontainer')
+<div id="Login">
+<h1>Login</h1>
+</div>
 
- 
+<div id="signup">
+    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
+    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
 </div>
 @stop
 
