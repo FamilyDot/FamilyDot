1c65f7b0ddf0a6ed75f1af086cf14dbeb21da56b
lo v <lov@los-MacBook-Pro.local>
2016-06-13 16:41:09 -0500

    changed how long the field input was

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -7,28 +7,28 @@
 
 @section('content')
 
-<div id="form">
-    <div id="link">
-    </div>
 
+
+<div id="form">
     <div id="signup-form">
             {{-- the action is temporary need to go back and redo --}}
         {{ Form::open(array('action' =>('HomeController@showLogin'), 'method' => 'POST')) }}
-
-
-        <div id="signup">
-            {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
-            {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
-            {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
-            {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Confirm Password')) }}
-            {{ Form::text('dateOfBirth',  null,  array('class' =>'form-control', 'placeholder'=> 'Date Of Birth')) }}
-            {{ Form::text('family',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name')) }}
-        </div>
-                <div id="button">
-                    <input class="btn btn-success" id="btn_sub"  type="submit" value="Signup">
+            <div class="row">
+                <div class="col-md-4  col-md-offset-4">
+                    {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
+                    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
+                    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
+                    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Confirm Password')) }}
+                    {{ Form::text('dateOfBirth',  null,  array('class' =>'form-control', 'placeholder'=> 'Date Of Birth')) }}
+                    {{ Form::text('family',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name')) }}
+                    <div id="button">
+                        <input class="btn btn-success" id="btn_sub"  type="submit" value="Signup">
+                            <br><a href="login">Have an account?</a>
+                    </div>
                 </div>
+            </div>
         {{ Form::close() }}
-        <h1><a href="login">Have an account?</h1>
+
     </div>
 </div>
 @stop
--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -8,21 +8,22 @@
 @section('content')
 
 <div id="form">
-
     <div id="login-form">
+            {{-- the action is temporary need to go back and redo --}}
         {{ Form::open(array('action' =>('HomeController@showHome'), 'method' => 'POST')) }}
-
-
-        <div id="signup">
-            {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
-            {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
-            {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
-        </div>
-                <div id="button">
-                    <input class="btn btn-success" id="btn_sub"  type="submit" value="Login">
+            <div class="row">
+                <div class="col-md-4  col-md-offset-4">
+                    {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username')) }}
+                    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
+                    {{ Form::text('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
+                    <div id="button">
+                        <input class="btn btn-success" id="btn_sub"  type="submit" value="Login">
+                            <br><a href="home">Don't have an account?</a>
+                    </div>
                 </div>
+            </div>
         {{ Form::close() }}
-        <h1><a href="home">Don't have an account?</h1>
+
     </div>
 </div>
 @stop
