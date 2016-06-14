2ac53c5d5bb554c85141794ef122b4fe1e58d92b
Will Campbell <sufuninja@gmail.com>
2016-06-14 14:23:12 -0500

    seeder for questions

--- app/database/seeds/QuestionsTableSeeder.php
+++ app/database/seeds/QuestionsTableSeeder.php
@@ -7,6 +7,40 @@ class QuestionsTableSeeder extends Seeder {
         DB::table('questions')->delete();
 
         $question = new Question();
+        $question->body = 'What activity this month did you enjoy the most?';
+        $question->user_id = 1;
+        $question->family_id = 1;
+        $question->save();
+        $question = new Question();
+        $question->body = 'What was one thing that really impresses you?';
+        $question->user_id = 2;
+        $question->family_id = 1;
+        $question->save();
+
+        $question = new Question();
+        $question->body = 'If you could change one thing about last week, what would it be?';
+        $question->user_id = 2;
+        $question->family_id = 1;
+        $question->save();
+
+        $question = new Question();
+        $question->body = 'What one thing about your family makes you the happiest?';
+        $question->user_id = 1;
+        $question->family_id = 1;
+        $question->save();
+
+        $question = new Question();
+        $question->body = 'What was something someone said that made you sad?';
+        $question->user_id = 1;
+        $question->family_id = 1;
+        $question->save();
+
+        $question = new Question();
+        $question->body = 'If you could go anywhere with your family, where would go?';
+        $question->user_id = 1;
+        $question->family_id = 1;
+        $question->save();
+
 
     }
 
