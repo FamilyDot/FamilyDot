53cf83df1091be93be60b8157f337c1ca275eb9e
Will Campbell <sufuninja@gmail.com>
2016-06-15 10:19:22 -0500

    Changed cloumn in answers table to 'answer' in place of 'body'

--- app/database/seeds/AnswersTableSeeder.php
+++ app/database/seeds/AnswersTableSeeder.php
@@ -7,25 +7,25 @@ class AnswersTableSeeder extends Seeder {
         DB::table('answers')->delete();
 
         $answer = new Answer();
-        $answer->body = 'I really liked going to the movies!';
+        $answer->answer = 'I really liked going to the movies!';
         $answer->user_id = 1;
         $answer->question_id = 1;
         $answer->save();
 
         $answer = new Answer();
-        $answer->body = 'Playing board games like Exploding Kittens.';
+        $answer->answer = 'Playing board games like Exploding Kittens.';
         $answer->user_id = 2;
         $answer->question_id = 1;
         $answer->save();
 
         $answer = new Answer();
-        $answer->body = 'Eating lunch at the park was great.';
+        $answer->answer = 'Eating lunch at the park was great.';
         $answer->user_id = 3;
         $answer->question_id = 1;
         $answer->save();
 
         $answer = new Answer();
-        $answer->body = 'I wish I would have studied for my math test more.';
+        $answer->answer = 'I wish I would have studied for my math test more.';
         $answer->user_id = 2;
         $answer->question_id = 2;
         $answer->save();
