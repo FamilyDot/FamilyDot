081fcbd36017cae47e3c1e83888539a7782c4cc3
lo v <lov@los-MacBook-Pro.local>
2016-06-16 13:21:01 -0500

    created the questions validator that included the rule(singular) for question

---
+++ app/validators/PostsValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+class PostsValidator
+{
+    public function validate($attributes)
+    {
+        $postsRules = array(
+            'body'             => 'required|max:3000',
+            
+        );
+
+        $validator = Validator::make($attributes, $postsRules);
+
+        if ($validator->fails()) {
+            throw new ValidationFailure('Could not post', $validator);
+        }
+    }
+}
