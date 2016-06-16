43b00c49f0d5da2f772416f3ff9f601bef3d544d
Will Campbell <sufuninja@gmail.com>
2016-06-16 13:59:39 -0500

    adding jquery function to grap id of question clicked and pass to form for createAnswer()

--- app/views/user.blade.php
+++ app/views/user.blade.php
@@ -15,7 +15,7 @@
     <div class='container' >
         <div id= 'pic_row' class= 'row'>
             <div class="col-md-3">
-                                   
+
                   <img src="uploads/kid1.jpg" alt="kid" id='user_pic'>
                     <div>
                         <h3>{{{ $user->username }}}</h3>
@@ -23,17 +23,31 @@
                         <h4>{{{ $user->last_name }}}</h4>
                         <h4>{{{ $user->email }}}</h4>
                         <h4>{{{ $user->birth_day }}}</h4>
-                    </div>     
+                    </div>
              </div>
                 <div class='col-md-8'>
                     <h1 class= 'container'>Question</h1>
                         @foreach($user->family->questions as $question)
                         <h2><a href="{{{ action('UsersController@show', $question->family_id)}}}">{{{ $question->question }}}</a></h2>
                     @endforeach
-                </div>    
+                </div>
         </div>
-    </div>  
-</div><!-- <-container div--> 
+    </div>
+</div><!-- <-container div-->
+@stop
+
+@section('bottomscript')
+<script type="text/javascript">
+$(document).ready(function(){
+    "use strict";
+
+    var $question_id = null;
+
+    $(".question").click(function(event) {
+      $question_id = event.target.id;
+    });
+})
+</script>
 @stop
 </body>
 
