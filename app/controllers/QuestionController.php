8b806fe9ca6f0455dc8cb795beb51d5348b52ae8
lo v <lov@los-MacBook-Pro.local>
2016-06-16 14:07:50 -0500

    added the conditionals and validations for the questions controller

--- app/controllers/QuestionController.php
+++ app/controllers/QuestionController.php
@@ -4,6 +4,10 @@ class QuestionController extends BaseController
 {
     public function store()
     {
+        $validator = new QuestionsValidator();
+        $validator->validate(Input::all());
+
+
         $user = Auth::user();
         $family_id = $user->family_id;
 
@@ -11,17 +15,30 @@ class QuestionController extends BaseController
         $question->question = Input::get('question');
         $question->family_id = $family_id;
         $question->user_id = $user->id;
-        $question->save();
-
-        return Redirect::action('UsersController@show');
-
+        
+        if ($validator->fails()) {
+            return Redirect::back();    
+        } else{
+            $question->save();
+            return Redirect::action('UsersController@show');
+        }
     }
 
     public function update($id)
     {
+        $validator = new QuestionsValidator();
+        $validator->validate(Input::all());
+
         $question = Question::find($id);
         $question->question = Input::get('question');
-        $question->save();
+        
+        if ($validator->fails()) {
+            return Redirect::back();    
+        } else{
+            $question->save();
+            return Redirect::action('UsersController@show');
+        }
+    }
 
         return Redirect::action('UsersController@show');
     }
