a720922c40b11d7904f35e82048cc8dd40909916
lo v <lov@los-MacBook-Pro.local>
2016-06-21 11:49:01 -0500

    added a column for survey questions

--- app/database/migrations/2016_06_13_195104_create_posts_table.php
+++ app/database/migrations/2016_06_13_195104_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration {
       {
           $table->increments('id');
           $table->string('body');
+          $table->string('survey_question');
           $table->integer('user_id')->unsigned();
           $table->foreign('user_id')->references('id')->on('users');
           $table->integer('family_id')->unsigned();
