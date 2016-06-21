c4e24a98baebb3536767e7fb0aacaeb08fa82fea
lo v <lov@los-MacBook-Pro.local>
2016-06-21 14:17:38 -0500

    refactored a modal for posts side of page

--- app/views/family.blade.php
+++ app/views/family.blade.php
@@ -21,7 +21,7 @@
                 <p>{{{ $post->body }}}</p>
             </div>
         @endforeach
-        <input class="btn btn-primary" id="sub_posts"  type="submit" value="Submit Posts">
+        <input class="btn btn-primary" id="sub_posts"  type="submit" value="Add to Posts">
     </div>
 
 
@@ -51,7 +51,35 @@
         <input class="btn btn-primary" id="sub_survey"  type="submit" value="Submit Survey">
     </div> 
 
-                 <!-- Modal -->
+                <!-- modal for posts -->
+            <div class="modal fade" id="myModalPost" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                                <h4 class="modal-title" id="myModalLabel">Create a Post?</h4>
+                        </div>                        
+                        <div class="modal-body">
+                            <textarea rows="4" cols="50">
+                            </textarea>
+                        </div>
+                        <!-- "modal-footer"> -->
+                        <div id="save_changes" class="modal-footer">
+                            <!-- <button type="button" id="close" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
+                            <button   type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                        <!-- </div> -->
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+                 <!-- Modal for survey -->
             <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
@@ -61,10 +89,6 @@
                             </button>
                                 <h4 class="modal-title" id="myModalLabel">Congrats your family's average health is <span id="health"></span>!</h4>
                         </div>
-                        <!-- <div class="modal-footer"> -->
-                            <button type="button" id="close" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
-                        <!-- </div> -->
                     </div>
                 </div>
             </div>
