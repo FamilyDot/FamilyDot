72e40b5dcaf893d38b5f7ca2151d491228c6f0d9
lo v <lov@los-MacBook-Pro.local>
2016-06-21 11:49:36 -0500

    added survey questions

--- app/database/seeds/PostsTableSeeder.php
+++ app/database/seeds/PostsTableSeeder.php
@@ -9,6 +9,7 @@ class PostsTableSeeder extends Seeder {
         $post = new Post();
         $post->body = 'Everytime I watch Deadpool, half way through I get the urge to pull out my comics and start reading.
             Also eat a chimichanga...or two, does anyone else agree?';
+        $post->survey_question = 'survey question 1';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -16,6 +17,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'Twelve-year-old Percy Jackson is on the most dangerous quest of his life. With the help of a satyr and a daughter of Athena, Percy must journey across the United States to catch a thief who has stolen the original weapon of mass destruction — Zeus’ master bolt... How does this summary rate?';
+        $post->survey_question = 'survey question 2';
         $post->user_id = '2';
         $post->family_id = '1';
         $post->img_url = 'http://orig04.deviantart.net/45f3/f/2013/159/1/a/percy_jackson_by_aireenscolor-d5fuvqv.jpg';
@@ -23,6 +25,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'Does everyone agree that tacos are pretty great';
+        $post->survey_question = 'survery question 3';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -30,6 +33,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'For our summer vacation does everyone agree that disneyworld is a good place to go?';
+        $post->survey_question = 'survey question 4';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -37,6 +41,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'Today i bought new shoes with some of our extra money for the week, I know we didn\'t agree that it was in our family budget, but don\'t you agree the flatter my legs?';
+        $post->survey_question = 'survey question 5';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -44,6 +49,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'this is a post, how would you rate this?';
+        $post->survey_question = 'survey question 6';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -52,6 +58,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'this post is post number 7 and i think you should rate this';
+        $post->survey_question = 'survey question 7';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -60,6 +67,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'How would you rate the ending of gilligans island';
+        $post->survey_question = 'survey question 8';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -68,6 +76,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'How would you rate the ending of ER';
+        $post->survey_question = 'survery question 9';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
@@ -76,6 +85,7 @@ class PostsTableSeeder extends Seeder {
 
         $post = new Post();
         $post->body = 'How would you rate the ending of Saved?';
+        $post->survey_question = 'survey question 10';
         $post->user_id = '1';
         $post->family_id = '1';
         $post->img_url = 'http://www.adweek.com/socialtimes/wp-content/uploads/sites/2/2016/02/DeadpoolLogo.jpg';
