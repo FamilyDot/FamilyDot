963df44fcc1999eb0c2c1aab36b145a4ce4293e8
lo v <lov@los-MacBook-Pro.local>
2016-06-21 11:50:00 -0500

    styled to include both posts and survey

--- app/views/family.blade.php
+++ app/views/family.blade.php
@@ -7,33 +7,50 @@
 @section('content')
 <div class="row">
     <div id="ms">
-    <h1 style='padding-top:70px;' class="container">{{{ $user->family->mission_statement }}}</h1>
-    <hr>
+        <h1 style='padding-top:70px;' class="container">{{{ $user->family->mission_statement }}}</h1>
+        <hr>
+    </div>
 
 
-            <div class="col-md-6 col-md-offset-3">
-                <form>
-                @foreach($user->family->posts as $post)
-                    <!-- Button trigger modal -->
-                    <div class="w3-card-4" id="card">
-                        <p>{{{ $post->body }}}</p>
-                        <div id="rad_btns">
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="1"> 1 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="2"> 2 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="3"> 3 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="4"> 4 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="5"> 5 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="6"> 6 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="7"> 7 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="8"> 8 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="9"> 9 
-                            <input type="radio" name="answers[{{$post->id}}]" class="number" value="10"> 10 
-                        </div>
+
+
+    <div class="col-md-4 col-md-offset-1">
+    <h1 id= "family_posts">Family Posts</h1> 
+        @foreach($user->family->posts as $post)
+            <div class="w3-card-4" id="card">
+                <p>{{{ $post->body }}}</p>
+            </div>
+        @endforeach
+        <input class="btn btn-primary" id="sub_posts"  type="submit" value="Submit Posts">
+    </div>
+
+
+
+    <div class="col-md-4 col-md-offset-1">
+    <h1 id="family_survey">Family Survey</h1>
+        <form>
+            @foreach($user->family->posts as $post)
+                <!-- Button trigger modal -->
+                <div class="w3-card-4" id="card">
+                    <p>{{{ $post->survey_question }}}</p>
+                    <div id="rad_btns">
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="1"> 1 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="2"> 2 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="3"> 3 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="4"> 4 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="5"> 5 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="6"> 6 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="7"> 7 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="8"> 8 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="9"> 9 
+                        <input type="radio" name="answers[{{$post->id}}]" class="number" value="10"> 10 
                     </div>
-                @endforeach
-                </form>
-                <input class="btn btn-primary" id="sub_survey"  type="submit" value="Submit Survey">
-            </div>    
+                </div>
+            @endforeach
+        </form>
+        <input class="btn btn-primary" id="sub_survey"  type="submit" value="Submit Survey">
+    </div> 
+
                  <!-- Modal -->
             <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
