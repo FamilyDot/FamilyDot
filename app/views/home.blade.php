910995c92e32e68ee7ff32c7392eb3a17b9f5976
lo v <lov@los-MacBook-Pro.local>
2016-06-24 14:37:41 -0500

    making edits for this form here ya'll

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -9,54 +9,65 @@
 <!-- need to relook at this -->
 <div class="container">
     <div class="row">
-        <div class="col-md-7">
+        <div class="col-md-4 col-md-offset-1">
             <img src="/img/Final-contestant.png">
         </div>
-        <div class="col-md-3">
-            {{ Form::open(array('action' =>('HomeController@doSignup'), 'method' => 'POST')) }}
-                <div class="form-group">
-                    {{ $errors->first('username', '<span class="help-block">:message</span>') }}
-                {{ Form::label('username', 'Username', array('class' => 'sr-only')) }}
-                {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username (Required)','id'=>'username-field')) }}
-                </div>
+        <div class="col-md-4 col-md-offset-1">
+            <div class="row">
+                {{ Form::open(array('action' =>('HomeController@doSignup'), 'method' => 'POST')) }}
+                    <div class="form-group">
+                        {{ $errors->first('username', '<span class="help-block">:message</span>') }}
+                        {{ Form::text('username',  null,  array('class' =>'form-control', 'placeholder'=> 'Username (Required)','id'=>'username-field')) }}
+                    </div>
+            </div>
+            <div class="row">
                 <div class="form-group">
                     {{ $errors->first('email', '<span class="help-block">:message</span>') }}
-                {{ Form::label('email', 'Email', array('class' => 'sr-only')) }}
-                {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email (Required)','id'=>'email-field')) }}
+                    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email (Required)','id'=>'email-field')) }}
                 </div>
+            </div>
+            <div class="row">
+                <div class="form-group"> <!-- relook at -->
+                    <div class="col-md-6">
+                        {{ $errors->first('password', '<span class="help-block">:message</span>') }}
+                        {{ Form::password('password',  array('class' =>'form-control password-field','placeholder'=> 'Password (Required)','id'=>'password-field')) }}
+                    </div> 
+                    <div class=" form-group col-md-6">
+                        {{ $errors->first('confirm password', '<span class="help-block">:message</span>') }}
+                        {{ Form::password('passwordValidate',  array('class' =>'form-control password-field','placeholder'=> 'Confirm Password (Required)','id'=>'passwordValidate-field')) }}
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class=" form-group">  
+                    <div class="col-md-6">
+                        {{ Form::text('first_name',  null,  array('class' =>'form-control','placeholder'=> 'First Name','id'=>'first_name-field')) }}
+                    </div>
+                    <div class="form-group col-md-6">
+                        {{ Form::text('last_name',  null,  array('class' =>'form-control','placeholder'=> 'Last Name','id'=>'last_name-field')) }}
+                    </div>
+                </div>
+            </div>
+            <div class="row">
                 <div class="form-group">
-                    {{ $errors->first('password', '<span class="help-block">:message</span>') }}
-                {{ Form::label('password', 'Password', array('class' => 'sr-only')) }}
-                {{ Form::password('password',  array('class' =>'form-control', 'placeholder'=> 'Password (Required)','id'=>'password-field')) }}
-              
-                    {{ $errors->first('confirm password', '<span class="help-block">:message</span>') }}
-                {{ Form::label('passwordValidate', 'PasswordValidate', array('class' => 'sr-only')) }}
-                {{ Form::password('passwordValidate',  array('class' =>'form-control', 'placeholder'=> 'Confirm Password (Required)','id'=>'passwordValidate-field')) }}
+                    {{ Form::text('birth_day',  null,  array('class' =>'form-control', 'placeholder'=> 'Birthday', 'id'=>'birth_day-field')) }}
                 </div>
+            </div>
+ 
+            <div class="row">
                 <div class="form-group">
-                {{ Form::label('birth_day', 'birth_day', array('class' => 'sr-only')) }}    
-                {{ Form::text('birth_day',  null,  array('class' =>'form-control', 'placeholder'=> 'Birthday', 'id'=>'birth_day-field')) }}
+                        {{ $errors->first('Family Name', '<span class="help-block">:message</span>') }}
+                    {{ Form::text('name',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name (Required)','id'=>'name-field')) }}
                 </div>
-
-                <div class="form-group">
-                {{ Form::label('first_name', 'first_name', array('class' => 'sr-only')) }}
-                {{ Form::text('first_name',  null,  array('class' =>'form-control', 'placeholder'=> 'First Name','id'=>'first_name-field')) }}
-                </div>
-                <div class="form-group">
-                {{ Form::label('last_name', 'last_name', array('class' => 'sr-only')) }}
-                {{ Form::text('last_name',  null,  array('class' =>'form-control', 'placeholder'=> 'Last Name','id'=>'last_name-field')) }}
-                </div>
-                <div class="form-group">
-                    {{ $errors->first('Family Name', '<span class="help-block">:message</span>') }}
-                {{ Form::label('name', 'name', array('class' => 'sr-only')) }}
-                {{ Form::text('name',  null,  array('class' =>'form-control', 'placeholder'=> 'Family Name (Required)','id'=>'name-field')) }}
-                </div>
+            </div>
+    
+            <div class="row">
                     <input class="btn btn-primary" id="btn_sub"  type="submit" value="Signup">
                     <p><a href="{{{action('HomeController@showLogin')}}}">Have an account?</a></p>
-                   
-            {{ Form::close() }}
+            </div>
+            {{ Form::close() }}     
         </div>
-    </div>
+    </div> <!-- end row -->
 </div>
 @stop
 
