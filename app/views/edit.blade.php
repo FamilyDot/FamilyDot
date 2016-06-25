4db2497cd3e193c9d868d833becd844fa889ef57
lo v <lov@los-MacBook-Pro.local>
2016-06-25 17:46:03 -0500

    tightened code

--- app/views/edit.blade.php
+++ app/views/edit.blade.php
@@ -9,48 +9,66 @@
 <h1>Edit Profile</h1>
 <div class="container">
     <div class="row">
-        <div class="col-md-5 col-md-offset-3">
-            {{ Form::open(array('action' =>('UsersController@update'), $user->id, 'method' => 'PUT')) }}
-               
-                <div class="form-group">
-                {{ Form::label('username', 'Username') }}
-                {{ Form::text('username',  null,  array('class'=>'form-control','id'=>'username-field')) }}
+        <div class="col-md-4 col-md-offset-4">
+            {{ Form::open(array('action' =>['UsersController@update', $user->id], 'method' => 'PUT')) }} 
+                
+                <div class="row">   
+                    <div class="form-group">
+                        {{ Form::label('username', 'Username') }}
+                        {{ Form::text('username', $user->username, ['class'=>'form-control']) }}
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="form-group">
+                        {{ Form::label('email', 'E-mail') }}
+                        {{ Form::text('email', $user->email, ['class'=>'form-control']) }}
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="form-group">
+                        {{ Form::label('image_url', 'Picture: Link a picture to your profile') }}
+                        {{ Form::text('image_url', "", ['class'=>'form-control','placeholder'=>'Paste a URL here!']) }}
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="form-group">
+                        {{ Form::label('twitter_username', 'Twitter User name') }}
+                        {{ Form::text('twitter_username', $user->twitter_username, ['class'=>'form-control']) }}
+                    </div>
                 </div>
 
-                <div class="form-group">
-                {{ Form::label('email', 'Email') }}
-                {{ Form::text('email',  null,  array('class'=>'form-control','id'=>'email-field')) }}
+                <div class="row"> 
+                    <div class="form-group">
+                        {{ Form::label('password', 'Password') }}
+                        {{ Form::password('password',  array('class'=>'form-control','placeholder'=>'Password (8 Characters Required)','id'=>'password-field')) }}
+                    </div>
+                </div>
+                <div class="row"> 
+                    <div class="form-group">
+                        {{ Form::label('password', 'Confirm Password') }}
+                        {{ Form::password('passwordValidate',  array('class'=>'form-control','placeholder'=>'Confirm Password (8 Characters Required)','id'=>'passwordValidate-field')) }}
+                    </div>
+                </div>
+                <div id="sub_btn">
+                    {{ Form::submit('submit', ['class' => 'btn btn-primary']) }}
                 </div>
 
-                <div class="form-group">
-                {{ Form::label('image_url', 'Picture: Link a picture to your profile') }}
-                {{ Form::text('image_url',  null, array('class'=>'form-control','placeholder'=>'Paste a URL here!','id'=>'image-field')) }}
-                </div>
-
-                <div class="form-group">
-                {{ Form::label('twitter_username', 'Twitter User name') }}
-                {{ Form::text('twitter_username', $user->twitter_username, array('class'=>'form-control','id' =>'twitter-field')) }}
-                </div>
-
-                <div class="form-group">
-                {{ Form::label('password', 'Password') }}
-                {{ Form::password('password',  array('class'=>'form-control','placeholder'=>'Password (8 Characters Required)','id'=>'password-field')) }}
-                </div>
-
-                <div class="form-group">
-                {{ $errors->first('confirm password', '<span class="help-block">:message</span>') }}
-                {{ Form::label('password', 'Confirm Password') }}
-                {{ Form::password('passwordValidate',  array('class'=>'form-control','placeholder'=>'Confirm Password (8 Characters Required)','id'=>'passwordValidate-field')) }}
-                </div>
-
-                <input class="btn btn-primary" id="btn_sub"  type="submit" value="Submit">
-                    {{ Form::open(['method' => 'DELETE', 'action' => ['UsersController@destroy', $user->id], 'onsubmit' => 'return ConfirmDelete()']) }}
-                    {{ Form::submit('Delete Profile', ['class' => 'btn-sm btn-danger']) }}
-                    {{ Form::close() }}
             {{ Form::close() }}
+
+            <div id="del_btn">
+                {{ Form::open(['method' => 'DELETE', 'action' => ['UsersController@destroy', $user->id], 'onsubmit' => 'return ConfirmDelete()']) }}
+                {{ Form::submit('Delete Profile', ['class' => 'btn btn-danger']) }}
             </div>
-        </div> <!-- row closed-->
+                {{ Form::close() }}
+        </div>
     </div>
+</div>
+
+
+
+
+
+
 
 
 <script>
